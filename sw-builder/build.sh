752342622beb9da2dde0ea940a26ad7e33045f65
Nathan Nguyen <159226326+Indigo5684@users.noreply.github.com>
2025-02-26 13:22:16 -0600

    Fix script

--- sw-builder/build.sh
+++ sw-builder/build.sh
@@ -4,6 +4,12 @@ BASEDIR=$(dirname "$0")
 BUILD_DIR="$BASEDIR/hexactrl-sw/build"
 CI_COMMIT_REF_NAME="778279a3"
 
+# Clone Repo
+if [! -d "$BASEDIR/hexactrl-sw" ]; then
+    git clone --recursive ssh://git@gitlab.cern.ch:7999/hgcal-daq-sw/hexactrl-sw.git "$BASEDIR/hexactrl-sw"
+end
+
+
 # Install dependencies
 echo "Install dependencies..."
 sudo yum install epel-release 
@@ -23,7 +29,7 @@ if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     cmake -DBUILD_CLIENT=ON -DBRANCH_NAME=$CI_COMMIT_REF_NAME -DCMAKE_INSTALL_PREFIX=/opt/hexactrl/$CI_COMMIT_REF_NAME ../
 else
     cmake -DBRANCH_NAME=$CI_COMMIT_REF_NAME -DCMAKE_INSTALL_PREFIX=/opt/hexactrl/$CI_COMMIT_REF_NAME ../
-end
+fi
 
 make -j3
 sudo make install
@@ -48,7 +54,7 @@ if [ ${MACHINE_TYPE} == 'x86_64' ]; then
 else
     sudo ln -s /opt/hexactrl/ROCv3/share/daq-server.service /etc/systemd/system/daq-server.service
     sudo ln -s /opt/hexactrl/ROCv3/share/i2c-server.service /etc/systemd/system/i2c-server.service
-end
+fi
 
 # Enable DAQ Client Service
 echo "Enabling DAQ Client Service..."
@@ -59,4 +65,4 @@ if [ ${MACHINE_TYPE} == 'x86_64' ]; then
 else
     sudo systemctl enable daq-server.service
     sudo systemctl enable i2c-server.service
-end
+fi
